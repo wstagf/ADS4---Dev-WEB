2975c409b01961ea721cbc080d87d499e2008a84
Thiago Augusto <wstagf@gmail.com>
2026-04-01 20:36:36 -0300

    alerta

--- Aula09/meuprimeiroprojeto/src/App.js
+++ Aula09/meuprimeiroprojeto/src/App.js
@@ -1,8 +1,13 @@
 function App() {
+  const alerta = () => {
+    alert("Olá Mundo");
+  }
+
   const nome = "Senai";
   return (
     <div className="App">
       <h1>{nome} Aula Web</h1>
+      <button onClick={alerta}>Clique aqui</button>
     </div>
   );
 }
