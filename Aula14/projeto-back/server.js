5af096b9ac7c21fa07ef022e969268eadec27360
Thiago Augusto <wstagf@gmail.com>
2026-05-06 19:48:01 -0300

    Foi movido as rotas de calc para dentro da pasta routes

--- Aula14/projeto-back/server.js
+++ Aula14/projeto-back/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const calcRoutes = require("./routes/calcRoutes");
 
 // =============================
 // MIDDLEWARES
@@ -13,64 +14,12 @@ app.get("/", (req, res) => {
   res.send("API rodando 🚀");
 });
 
-
-// rota de teste
-app.post("/somar", (req, res) => {
-  x = req.body.x;
-  y = req.body.y;
-  console.log(`Recebido: x=${x}, y=${y}`);
-  resultado = x + y;
-  res.send({ resultado });
-});
-
-
-// rota de teste
-app.post("/somarQuery", (req, res) => {
-  const x = parseFloat(req.query.x) || 0;
-  const y = parseFloat(req.query.y) || 0;
-  console.log(`Recebido: x=${x}, y=${y}`);
-  resultado = x + y;
-  res.send({ resultado });
-});
-
-
-// rota de teste
-app.post("/calc/:metodo", (req, res) => {
-
-  const x = parseFloat(req.query.x) || 0;
-  const y = parseFloat(req.query.y) || 0;
-  console.log(`Recebido: x=${x}, y=${y}`);
-
-  resultado = 0;
-
-  switch (req.params.metodo) {
-    case "somar":
-        resultado = x + y;
-        break;
-    case "subtrair":
-        resultado = x - y;
-        break;
-    case "multiplicar":
-        resultado = x * y;
-        break;
-    case "dividir":
-        resultado = y !== 0 ? x / y : "Erro: divisão por zero";
-        break;
-
-    default:
-        resultado = "Método desconhecido";
-        break;
-  } 
-  res.send({ resultado });
-});
-
+app.use(calcRoutes);
 
 listaDeUsuarios = [];
 app.get("/usuarios", (req, res) => {
   res.send(listaDeUsuarios);
 });
-
-
 
 app.post("/usuarios", (req, res) => {
   const { nome, email } = req.body;
@@ -82,7 +31,7 @@ app.post("/usuarios", (req, res) => {
   res.send({ mensagem: `queryParamter ${queryParamter} ` });
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API rodando em http://192.168.137.1:${PORT}`);
